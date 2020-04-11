31be54ba817d50bb16b4a4f354087d2c23796a39
Sakthivel <sakthivelsekar33@gmail.com>
2020-04-11 20:30:44 +0530

    vue-xlsx-format-filters

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var VueXlsxFormatFilters = {
+  install: function(vue, options) {
+    console.log('Installing VueXlsxFormatFilters')
+    console.log(vue, options)
+  }
+}
+
+export default VueXlsxFormatFilters
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueXlsxFormatFilters from './index'
 
 Vue.config.productionTip = false
+Vue.use(VueXlsxFormatFilters)
 
 new Vue({
   render: h => h(App),
